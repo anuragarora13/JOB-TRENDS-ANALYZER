43d3f6e4d287d1138a4b0f19af0f4025bb205b1b
Anurag <aroraanurag647@gmail.com>
2024-10-21 23:08:03 +0530

    Second Comit

---
+++ src/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+        print(f"Job Title: {job_title}")
+        print(f"Company: {job_company}")
+        print(f"Location: {job_location}")
+        print("------")
--- src/web_scrapper.py
+++ src/web_scrapper.py
@@ -1,20 +1,30 @@
-import requests
+# Build a project that scrapes websites for data and analyzes it. 
+# For example, scrape job listings to study trends.
+# import requests
+
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
-url='https://jobspresso.co/remote-ai-data-jobs/'
-response=requests.get(url)
-
-if response.status_code==200:
-    print("Successfully Fetched The Web page")
-    html_comntent=response.content
-else:
-    print(f"Failed to fetch the web page  {response.status_code} ")
-       
-soup= BeautifulSoup(response.content,'html.parser')    
-
-job_titles=soup.find_all(class_='job_listings')
-
-for job in job_titles:
-    print(job.text.strip())
+with sync_playwright() as p:
     
+    browser=p.chromium.launch(headless=True)
     
+    page=browser.new_page()
+    
+    page.goto('https://jobspresso.co/remote-ai-data-jobs/')
+    
+    page.wait_for_load_state('networkidle')
+    html_content = page.content()
+    
+    # soup= BeautifulSoup(html_content,'html.parser')  
+    # job_listings=soup.find_all(class_='job_listings')
+    job_elements = page.locator('li.job_listing')  # Adjust this selector if necessary
+    for i in range(job_elements.count()):
+        job_title = job_elements.nth(i).locator('h3.job_listing-title').inner_text()
+        job_company = job_elements.nth(i).locator('div.job_listing-company').inner_text()
+        job_location = job_elements.nth(i).locator('div.job_listing-location').inner_text()
+        
+        print(f"Job Title: {job_title}")
+        print(f"Company: {job_company}")
+        print(f"Location: {job_location}")
+        print("------")
